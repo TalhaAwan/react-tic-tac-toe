3290ec0263c30b0d3032e933eeda0b9296652762
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-20 00:05:00 +0500

    taking turns

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ import './index.css';
 
 
 function Square(props) {
-  render() {
     return (
-      <button className="square" onClick={props.onClick})}>
+      <button className="square" onClick={props.onClick}>
         {props.value}
       </button>
     );
-  }
+  
 }
 
 class Board extends React.Component {
   constructor(){
     super();
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
+
   renderSquare(i) {
     return <Square 
     value={this.state.squares[i]} 
@@ -29,11 +30,14 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext? 'X': 'O');
 
     return (
       <div>
