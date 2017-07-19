de8829777590fb491a21a8dcb80030dd08a78911
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-20 00:42:39 +0500

    add constructor in game class

--- src/index.js
+++ src/index.js
@@ -73,6 +73,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    }
+  }
   render() {
     return (
       <div className="game">
