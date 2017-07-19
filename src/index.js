0683db979fa25502f55a2be8d44ffa6c37608650
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-19 23:43:32 +0500

    lifting state up (controlled components)

--- src/index.js
+++ src/index.js
@@ -4,26 +4,32 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      value: null
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'x'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()})}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square 
+    value={this.state.squares[i]} 
+    onClick={() => this.handleClick(i)}
+    />
   }
 
+  handleClick(i){
+    const squares = this.state.squares.slice();
+  }
   render() {
     const status = 'Next player: X';
 
