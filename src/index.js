03f59cb9ce03a687d5408680d37b962d18ad9484
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-20 22:53:43 +0500

    Bold the currently-selected item in the move list.

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ class Game extends React.Component {
     const moves = history.map((step, move) => {
       const desc = move? 'Move #' + move: 'Game Start';
       return (
-        <li key={move}>
+        <li key={move} style={move == this.state.stepNumber? {fontWeight: 600}: null}>
           <a href="#" onClick={() => this.jumpTo(move)}> {desc} </a>
         </li>
       );
